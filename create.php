2660a63209bde8bae93832fa6e5039488aaa6a2e
Pedro Neves <contato.nevespedro@gmail.com>
2022-11-28 17:46:23 -0300

    adicionado link css e script do Bootstrap

--- create.php
+++ create.php
@@ -5,9 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CRUD</title>
-  <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/navbar-fixed/">
-  <link href="./assets/bootstrap.min.css" rel="stylesheet">
-  <link href="./assets/navbar-top-fixed.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
@@ -39,7 +37,7 @@
         <button type="submit" class="btn btn-primary">Salvar</button>
       </form>
   </main>
-  <script src="/CRUD_unipe/assets/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- exibeDados.php
+++ exibeDados.php
@@ -19,9 +19,8 @@ if ($sql->rowCount() > 0) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CRUD</title>
-  <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/navbar-fixedaaa/">
-  <link href="./assets/bootstrap.min.css" rel="stylesheet">
-  <link href="./assets/navbar-top-fixed.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  
 </head>
 
 <body>
@@ -62,7 +61,7 @@ if ($sql->rowCount() > 0) {
         </tbody>
       </table>
   </main>
-  <script src="/CRUD_unipe/assets/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
