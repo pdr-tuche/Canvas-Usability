467773ff44cc821cd86e9fdfde506787a3745a20
Pedro Neves <contato.nevespedro@gmail.com>
2022-11-29 03:23:42 -0300

    colcado tag php no body

--- create.php
+++ create.php
@@ -11,7 +11,7 @@
 <body>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark ">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Adicionar Cliente</a>
+      <a class="navbar-brand" href="#"><?php echo "Adicionar Cliente" ?></a>
     </div>
   </nav>
 
